203dc7061036feaf3d3bc3f2fd1363df4b6da8e6
IvanSaeco <ivan.saenz@unmsm.edu.pe>
2023-08-30 14:17:30 -0500

    Colab: Conceptos básicos de Compose 3, se adiciono un color de letra alterno (azul) debido a problemas de visualización durante el modo oscuro del celular de prueba.

---
+++ CuadranteCompose_20200095_20200007/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CuadranteCompose_20200095_20200007"
+include(":app")
